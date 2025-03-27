4dd4f998dc5b0ece417d8d167187c834cec60025
Misha <darkrabbit240@gmail.com>
2025-03-27 23:19:21 +0200

    feat: add readline

---
+++ sources/readline.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readline.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 21:55:14 by mlitvino          #+#    #+#             */
+/*   Updated: 2025/03/27 23:16:24 by mlitvino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	read_input(int argc, char *argv[], char *env[])
+{
+	char	*read_line;
+
+	while (1)
+	{
+		read_line = readline("minishell$ ");
+		add_history(read_line);
+		if (ft_strcmp(read_line, "exit") == 0)
+			ft_exit();
+
+		free(read_line);
+	}
+}
