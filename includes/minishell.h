ddf2e34d8dda83e5dd93569c4c40e506aedade17
Misha <darkrabbit240@gmail.com>
2025-03-29 11:10:04 +0200

    feat: add draft of core structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 15:29:19 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/03/27 23:39:27 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/03/29 11:08:54 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,27 @@
 # include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+
+typedef struct s_file
+{
+	char	*path_name;
+	int		fd;
+	int		mod; // echo 123 > file - overwrite file. echo 123 >> file - add text to end
+}	t_file;
+
+typedef struct s_cmd_tab
+{
+	char	*cmd1;
+	char	**pipes_cmd;
+	t_file	*infile;
+	t_file	*outfile;
+}	t_cmd_tab;
+
+typedef struct s_data
+{
+	t_cmd_tab	**cmd_flow; /* (infile < cmd1 | cmd 2 | cmd3 > outfile) - 1 flow
+							(cmd1 | cmd2) && (cmd1 > outfile) - 2 flows*/
+}	t_data;
 
 // readline.c
 void	read_input(int argc, char *argv[], char *env[]);
