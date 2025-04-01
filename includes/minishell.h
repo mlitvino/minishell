4c3c0d317a6fa58aeca499974ebff9a3026b7440
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-04-01 18:26:20 +0300

    feat: change main struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 15:29:19 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/04/01 12:33:00 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/04/01 18:25:39 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@
 # include <readline/history.h>
 # include <readline/readline.h>
 
+# define STDIN 0
+# define STDOUT 1
+#define STDERR 2 // do we need it?
+
+typedef struct s_pipe
+{
+	long	pipe[2];
+}	t_pipe;
+
 typedef struct s_file
 {
 	char	*path_name;
@@ -28,22 +37,26 @@ typedef struct s_file
 
 typedef struct s_cmd
 {
+	char	*cmd_path;
 	char	*cmd_name;
 	char	*optns;
 	int		wildcard;
+	char	**arg_list; // cmd_name + optns + args
+	t_file	*infile;
+	t_file	*outfile;
 }	t_cmd;
 
 typedef struct s_cmd_tab
 {
 	t_cmd	*cmds;
-	t_file	*infile;
-	t_file	*outfile;
+	int		cmd_count;
 	int		exit_code;
+	t_pipe	*pipes;
 }	t_cmd_tab;
 
 typedef struct s_data
 {
-	t_cmd_tab	*cmd_flow;
+	t_cmd_tab	*cmd_flows;
 }	t_data;
 
 // readline.c
