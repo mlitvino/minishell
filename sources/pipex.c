f5c5ccbde61e137b7350b5a3a591bea33967f395
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-04-01 18:25:13 +0300

    feat: change structs, add redirects, pipes

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,81 +6,73 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 12:03:21 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/03/31 17:01:29 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/04/01 17:36:49 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	pipex(char *read_line, char *env[])
-{
-	int		pipefd[2];
-	pid_t	child_pid;
-	int		outfile;
-	int		infile;
-	char	*line;
+// void	pipex(char *read_line, char *env[])
+// {
+// 	int		pipefd[2];
+// 	pid_t	child_pid;
+// 	int		outfile;
+// 	int		infile;
+// 	char	*line;
 
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		outfile = open("te.txt", O_RDWR | O_TRUNC);
-		infile = open("test.txt", O_RDWR);
-		int	tempin = dup(0);
-		int	tempout = dup(1);
-		pipe(pipefd);
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			close(pipefd[0]);
-			dup2(infile, 0);
-			dup2(pipefd[1], 1);
+// 	child_pid = fork();
+// 	if (child_pid == 0)
+// 	{
+// 		outfile = open("te.txt", O_RDWR | O_TRUNC);
+// 		infile = open("test.txt", O_RDWR);
+// 		pipe(pipefd);
+// 		child_pid = fork();
+// 		if (child_pid == 0)
+// 		{
+// 			close(pipefd[0]);
+// 			dup2(infile, 0);
+// 			dup2(pipefd[1], 1);
 
 
-		close(tempin);
-		close(tempout);
-		close(outfile);
-		close(infile);
-		close(pipefd[0]);
-		close(pipefd[1]);
+// 		close(outfile);
+// 		close(infile);
+// 		close(pipefd[0]);
+// 		close(pipefd[1]);
 
-			char *argv[] = {"grep", "test", NULL};
-			execve("/bin/grep", argv, env);
-			perror("execve:");
-			exit(1);
-		}
-		else
-		{
-			close(pipefd[1]);
-			dup2(outfile, 1);
-			dup2(pipefd[0], 0);
+// 			char *argv[] = {"cat", NULL};
+// 			execve("/bin/cat", argv, env);
+// 			perror("execve:");
+// 			exit(1);
+// 		}
+// 		else
+// 		{
+// 			close(pipefd[1]);
 
 
+// 			dup2(pipefd[0], 0);
+// 			dup2(outfile, 1);
 
-		close(tempin);
-		close(tempout);
-		close(outfile);
-		close(infile);
-		close(pipefd[0]);
-		close(pipefd[1]);
+// 		close(outfile);
+// 		close(infile);
+// 		close(pipefd[0]);
+// 		close(pipefd[1]);
 
-			char *argv2[] = {"grep", "test", NULL};
-			execve("/bin/grep", argv2, env);
-			perror("eeexecve");
-			exit(1);
-		}
+// 			char *argv2[] = {"cat", NULL};
+// 			execve("/bin/cat", argv2, env);
+// 			perror("eeexecve");
+// 			exit(1);
+// 		}
 
 
-		close(tempin);
-		close(tempout);
-		close(outfile);
-		close(infile);
-		close(pipefd[0]);
-		close(pipefd[1]);
+// 		close(outfile);
+// 		close(infile);
+// 		close(pipefd[0]);
+// 		close(pipefd[1]);
 
-		cmd_exit();
-	}
-	else
-	{
-		waitpid(child_pid, 0, 0);
-	}
-}
+// 		cmd_exit();
+// 	}
+// 	else
+// 	{
+// 		waitpid(child_pid, 0, 0);
+// 	}
+// }
--- sources/readline.c
+++ sources/readline.c
@@ -6,11 +6,92 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 21:55:14 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/03/31 16:04:44 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/04/01 18:19:13 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	redirect_close_fd(t_cmd *cmd, t_cmd_tab *cmd_flow, int cmd_i)
+{
+	if (cmd->infile)
+	{
+		cmd->infile->fd = open(cmd->infile->path_name, O_RDONLY);
+		dup2(cmd->infile->fd, STDIN);
+		close(cmd->infile->fd);
+	}
+	else if (cmd_i != 0)
+	{
+		dup2(cmd_flow->pipes[cmd_i - 1].pipe[STDIN], STDIN);
+	}
+
+
+	if (cmd->outfile)
+	{
+		cmd->outfile->fd = open(cmd->outfile->path_name, O_WRONLY);
+		dup2(cmd->outfile->fd, STDOUT);
+		close(cmd->outfile->fd);
+	}
+	else if (cmd_flow->cmd_count != 1)
+	{
+		dup2(cmd_flow->pipes[cmd_i - 1].pipe[STDOUT], STDOUT);
+	}
+
+	int i = 0;
+	while (i < cmd_flow->cmd_count - 1)
+	{
+		close(cmd_flow->pipes[i].pipe[STDIN]);
+		close(cmd_flow->pipes[i].pipe[STDOUT]);
+	}
+}
+
+void	run_cmd(t_cmd *cmd, t_cmd_tab *cmd_flow, int cmd_i)
+{
+	char	*env[0]; // test
+
+	redirect_close_fd(cmd, cmd_flow, cmd_i);
+	execve(cmd->cmd_path, cmd->arg_list, env);
+	//err check
+	exit(1);
+}
+
+int	pipex(t_cmd_tab *cmd_flow)
+{
+	int		i;
+	pid_t	chld_pid;
+
+	cmd_flow->cmd_count = 3; //test
+
+	//pipes init
+	cmd_flow->pipes = malloc(sizeof(t_pipe) * cmd_flow->cmd_count - 1);
+	int i = 0;
+	while (i < cmd_flow->cmd_count - 1)
+	{
+		pipe(cmd_flow->pipes[i].pipe);
+		i++;
+	}
+
+	i = 0;
+	while (i < cmd_flow->cmd_count)
+	{
+		chld_pid = fork();
+		if (chld_pid == -1)
+		{
+			//err check
+		}
+		if (chld_pid == 0)
+		{
+			run_cmd(&cmd_flow->cmds[i], cmd_flow, i);
+		}
+		else
+		{
+
+		}
+		i++;
+	}
+	waitpid(chld_pid, 0, 0); // check exit status child
+	return (cmd_flow->exit_code);
+}
 
 void	read_input(int argc, char *argv[], char *env[])
 {
@@ -23,8 +104,10 @@ void	read_input(int argc, char *argv[], char *env[])
 		if (ft_strcmp(read_line, "exit") == 0)
 			cmd_exit();
 
-		if (read_line)
-			pipex(read_line, env);
+		// if (read_line)
+		// 	pipex(read_line, env);
+		t_cmd_tab cmd_flow;
+		pipex(&cmd_flow);
 
 		free(read_line);
 	}
