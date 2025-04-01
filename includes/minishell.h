ad66ca1e2ce9ce8635aaa9e950d116d4ce2039b6
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-04-01 12:33:53 +0300

    feat: change struts

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 15:29:19 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/03/29 11:08:54 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/04/01 12:33:00 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,27 @@ typedef struct s_file
 {
 	char	*path_name;
 	int		fd;
-	int		mod; // echo 123 > file - overwrite file. echo 123 >> file - add text to end
+	int		mod;
 }	t_file;
+
+typedef struct s_cmd
+{
+	char	*cmd_name;
+	char	*optns;
+	int		wildcard;
+}	t_cmd;
 
 typedef struct s_cmd_tab
 {
-	char	*cmd1;
-	char	**pipes_cmd;
+	t_cmd	*cmds;
 	t_file	*infile;
 	t_file	*outfile;
+	int		exit_code;
 }	t_cmd_tab;
 
 typedef struct s_data
 {
-	t_cmd_tab	**cmd_flow; /* (infile < cmd1 | cmd 2 | cmd3 > outfile) - 1 flow
-							(cmd1 | cmd2) && (cmd1 > outfile) - 2 flows*/
+	t_cmd_tab	*cmd_flow;
 }	t_data;
 
 // readline.c
