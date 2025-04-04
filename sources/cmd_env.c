5b0ce300d732cf5bdf60f878034e2c4f4e28eb61
Misha <darkrabbit240@gmail.com>
2025-04-05 01:23:00 +0300

    fix: delete extra var and lines

--- sources/cmd_env.c
+++ sources/cmd_env.c
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:43:02 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/04/04 18:10:36 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/04/04 22:57:54 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	cpy_env(char *sys_env[], t_data *data)
 {
 	int			i;
-	size_t		len;
 	t_list		*new_env;
 	t_list		*new_node;
 	char		*content;
@@ -36,10 +35,9 @@ void	cpy_env(char *sys_env[], t_data *data)
 
 void	cmd_env(t_list *env)
 {
-	while (env->next)
+	while (env)
 	{
 		printf("%s\n", (char *)env->content);
 		env = env->next;
 	}
-	printf("%s\n", (char *)env->content);
 }
