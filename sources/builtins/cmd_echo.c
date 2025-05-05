bce530e6d40f20b2afad2c222756713cd5320e55
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-05-05 18:44:32 +0300

    feat: enhance cmd_echo to handle newline

--- sources/builtins/cmd_echo.c
+++ sources/builtins/cmd_echo.c
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 13:35:42 by mlitvino          #+#    #+#             */
-/*   Updated: 2025/05/01 15:49:28 by mlitvino         ###   ########.fr       */
+/*   Updated: 2025/05/05 18:08:30 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,58 @@ int	print_args(t_data *data, t_args *args, int newlne)
 	return (SUCCESS);
 }
 
+int	get_i_newline(char *str)
+{
+	int	i;
+	int	optn_flg;
+
+	i = 0;
+	optn_flg = 0;
+	while (str[i])
+	{
+		if (str[i] == '-' && str[i + 1] == 'n')
+			while (str[++i] == 'n')
+				optn_flg |= 1;
+		else if (ft_isspace(str[i]) == 1)
+			while (ft_isspace(str[++i]) == 1)
+				{ }
+		else
+			break ;
+	}
+	if (optn_flg == 1 && !str[i])
+		return (-1);
+	else if (optn_flg == 1 && str[i])
+		return (i);
+	return (optn_flg);
+}
+
+int	check_split_arg(t_data *data, t_args *args)
+{
+	t_args	*temp;
+	int		newlne;
+	int		res;
+
+	res = get_i_newline(args->value);
+	temp = args;
+	newlne = res;
+	if (temp && (newlne != -1 || newlne != 0))
+	{
+		// trim_extra_optn
+		temp = args->next;
+		get_i_newline(temp->value);
+	}
+	return (res);
+}
+
 int	cmd_echo(t_data *data, t_args *args)
 {
-	int	exit_code;
-	int	newlne;
+	int		exit_code;
+	int		newlne;
 
 	exit_code = 0;
-	newlne = is_new_line(data, args);
+	newlne = 0;
+	if (args)
+		newlne = check_split_arg(data, args);
 	if (!args || newlne == SUCCESS)
 	{
 		exit_code |= print_args(data, args, newlne);
